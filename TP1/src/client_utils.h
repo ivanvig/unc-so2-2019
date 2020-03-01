b0c8f392c2c3cb10dbad8a9c4c70d91d2de14120
ivanvig <ivanmvig@gmail.com>
2020-03-01 12:51:32 -0300

    FIX: path relativo a la imagen

--- TP1/src/client_utils.h
+++ TP1/src/client_utils.h
@@ -4,7 +4,7 @@
 
 #define SOFT_VER "1.0"
 #define FPATH                                                                  \
-	"/home/ivan/Projects/Facultad/unc-so2-2019/TP1/misc/20190861730_GOES16-ABI-FD-GEOCOLOR-10848x10848.jpg"
+	"./misc/20190861730_GOES16-ABI-FD-GEOCOLOR-10848x10848.jpg"
 #define RCV_UPD_FNAME "firmware_update"
 #define UPD_SCRIPT "update_binary.sh"
 #define CLIENT_FPORT 6666
